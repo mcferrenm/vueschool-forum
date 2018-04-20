dfa50edfcb620b99aa7eea8990f71afdd6963e32
mcferrenmax <mcferrenmax@gmail.com>
2018-04-20 13:58:36 -0400

    routing

---
+++ src/pages/PageHome.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="col-full">
+    <h1>Welcome To The Forum!</h1>
+    <ThreadList :threads="threads"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import sourceData from '@/data'
+import ThreadList from '@/components/ThreadList'
+console.log(sourceData)
+
+export default {
+  components: {
+    ThreadList
+  },
+
+  data () {
+    return {
+      threads: Object.values(sourceData.threads)  ,
+      posts: sourceData.posts,
+      users: sourceData.users
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
