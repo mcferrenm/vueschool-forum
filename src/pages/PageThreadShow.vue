6ed4eab96f92113bc7e94f5b9e76b10c8f31a83d
mcferrenmax <mcferrenmax@gmail.com>
2018-04-29 17:55:09 -0400

    added thread editor

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -1,7 +1,16 @@
 <template>
 	<div>
     <div class="col-large push-top">
-        <h1>{{thread.title}}</h1>
+        <h1>{{thread.title}}
+
+					<router-link
+						:to="{name: 'ThreadEdit', id: this.id}"
+						class="btn-green btn-small"
+						tag="button"
+					>
+						Edit Thread
+					</router-link>
+				</h1>
 				<p>
 					By <a href="#" class="link-unstyled">Robin</a>, <AppDate :timestamp="thread.publishedAt"/>
 					<span style="float:right; margin-top: 2px;" class="hide-mobile text-faded text-small">3 replies by 3 contributors</span>
@@ -43,3 +52,7 @@
 		}
 	}
 </script>
+
+<style scoped>
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -21,28 +21,47 @@ export default new Vuex.Store({
       commit('setPost', {post, postId})
       commit('appendPostToThread', {threadId: post.threadId, postId})
       commit('appendPostToUser', {userId: post.userId, postId})
+      return Promise.resolve(state.posts[postId])
     },
 
-    createThread({ state, commit, dispatch }, { text, title, forumId }) {
+    createThread ({ state, commit, dispatch }, { text, title, forumId }) {
       return new Promise((resolve, reject) => {
         const threadId = 'greatThread' + Math.random()
         const userId = state.authId
         const publishedAt = Math.floor(Date.now() / 1000)
 
-        const thread = { '.key': threadId, title, forumId, publishedAt, userId }
+        const thread = {'.key': threadId, title, forumId, publishedAt, userId}
 
         commit('setThread', { threadId, thread })
         commit('appendThreadToForum', { forumId, threadId })
         commit('appendThreadToUser', { userId, threadId })
 
         dispatch('createPost', { text, threadId })
+          .then(post => {
+            commit('setThread', {threadId, thread: {...thread, firstPostId: post['.key']}})
+          })
         resolve(state.threads[threadId])
         })
       },
 
-    updateUser ({commit}, user) {
-      commit('setUser', {userId: user['.key'], user})
-    }
+      updateThread ({state, commit}, {title, text, id}) {
+        return new Promise ((resolve, reject) => {
+        const thread = state.threads[id]
+        const post = state.posts[thread.firstPostId]
+
+        const newThread = {...thread, title}
+        const newPost = {...post, text}
+
+        commit('setThread', {thread: newThread, threadId: id})
+        commit('setPost', {post: newPost, postId: thread.firstPostId })
+
+        resolve(newThread)
+        })
+      },
+
+      updateUser ({commit}, user) {
+        commit('setUser', {userId: user['.key'], user})
+      }
   },
 
   getters: {
