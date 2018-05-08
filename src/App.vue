4cf11ec9709bd38eb2a42c12419e894011825a41
mcferrenmax <mcferrenmax@gmail.com>
2018-05-08 11:50:59 -0400

    added loading spinner

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,32 @@
   <div id="app">
     <TheNavBar/>
     <div class="container">
-      <router-view/>
+      <router-view v-show="showPage" @ready="showPage = true"/>
+      <AppSpinner v-show="!showPage"/>
     </div>
   </div>
 </template>
 
 <script>
 import TheNavBar from '@/components/TheNavBar'
+import AppSpinner from '@/components/AppSpinner'
 export default {
   components: {
-    TheNavBar
+    TheNavBar,
+    AppSpinner
+  },
+
+  data () {
+    return {
+      showPage: false
+    }
+  },
+
+  created () {
+    this.$router.beforeEach((to, from, next) => {
+      this.showPage = false
+      next()
+    })
   }
 }
 </script>
